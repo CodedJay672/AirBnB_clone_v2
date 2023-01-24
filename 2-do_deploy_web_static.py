1acfebe268c6bc6cba28c0eeeff6cadd40e15158
codedjay672 <emmajoel672@gmail.com>
2023-01-24 17:21:31 +0000

    update do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,7 +19,8 @@ def do_deploy(archive_path):
 
     if put(archive_path, "/tmp/{}".format(fpath)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".format(fname)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".format(
+           fname)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".format(
            fname)).failed is True:
@@ -38,7 +39,7 @@ def do_deploy(archive_path):
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(
-           fname)).failed is True:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(fname)).failed is True:
         return False
     return True
