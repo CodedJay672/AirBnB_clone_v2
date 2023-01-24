9b23e8e443a5392e4acc326cc4349dff7beaf968
codedjay672 <emmajoel672@gmail.com>
2023-01-24 12:28:16 +0000

    upload tgz archive file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# python file that archives files in web static folder
+
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    date = datetime.utcnow()
+    tarFile = "versions/web_static_{}{}{}{}{}.tgz".format(
+                                                          date.year,
+                                                          date.month,
+                                                          date.day,
+                                                          date.hour,
+                                                          date.minute,
+                                                          date.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(tarFile)).failed is True:
+        return None
+    return tarFile
