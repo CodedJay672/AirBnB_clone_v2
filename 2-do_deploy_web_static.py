03d5cfc7b583d4c63f46d0150284514206e7821f
codedjay672 <emmajoel672@gmail.com>
2023-01-24 15:43:44 +0000

    upload archived file with fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ from fabric.api import put
 
 env.hosts = ["52.91.132.254", "100.25.203.14"]
 
+
 def do_deploy(archive_path):
     """function that uploads an archive file to a web server"""
 
@@ -16,18 +17,28 @@ def do_deploy(archive_path):
     fpath = archive_path.split("/")[-1]
     fname = fpath.split(".")[0]
 
-    if put(archive_path, "/tmp/fpath").failed is True:
+    if put(archive_path, "/tmp/{}".format(fpath)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/fname").failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".format(fname)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/fname").failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".format(
+           fname)).failed is True:
         return False
-    if run("tar -xzf /tmp/fpath -C /data/web_static/releases/fname").failed is True:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+           fpath, fname)).failed is True:
         return False
-    if run("rm -rf /tmp/fpath").failed is True:
+    if run("rm /tmp/{}".format(fpath)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(fname, fname)
+           ).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static"
+           .format(fname)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/fname /data/web_static/current").failed is True:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(
+           fname)).failed is True:
         return False
     return True
